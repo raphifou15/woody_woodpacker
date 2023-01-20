f01d7d70345d247fc23afb9877c47c6ded71325a
rkhelif <raphi031@hotmail.fr>
2023-01-20 16:29:21 +0100

    toujour sur la comprehension d'un fichier elf

--- debug.c
+++ debug.c
@@ -18,19 +18,75 @@ void printStats(struct stat *sb){
 
 void printFileHexa(char *file, size_t size){
 	char	*lala;
+	size_t	nb;
+	size_t	nb2;
 	
+	(void)size;
 	lala = (char *)file;
-  for (size_t i = 0; i < size; i++)
-    printf("%x", lala[i]);
-  printf("\n");
+  for (size_t i = 0; i < 64; i++)
+	{
+		if (i == 0) printf("Elf64_Ehdr\n 16 byte e_ident: ");
+		if (i == 16) printf("2 byte e_type: ");
+		if (i == 18) printf("2 byte e_machine: ");
+		if (i == 20) printf("4 byte e_version: ");
+		if (i == 24) printf("8 byte e_entry: ");
+		if (i == 32) printf("8 byte e_phoff: ");
+		if (i == 40) printf("8 byte e_shoff: ");
+		if (i == 48) printf("4 byte e_flags: ");
+		if (i == 52) printf("2 byte e_ehsize: ");
+		if (i == 54) printf("2 byte e_phentsize: ");
+		if (i == 56) printf("2 byte e_phnum: ");
+		if (i == 58) printf("2 byte e_shentsize: ");
+		if (i == 60) printf("2 byte e_shnum: ");
+		if (i == 62) printf("2 byte e_shstrndx: ");
+		if (lala[i] < 16)
+			printf("0");
+    printf("%x ", lala[i]);
+		if (i == 15 || i == 17 || i == 19 || i == 23 || i == 31 || i == 39 ||
+				i == 47 || i == 51 || i == 53 || i == 55 || i == 57 || i == 59 ||
+				i == 61 || i == 63) printf("\n");
+	}
+	printf("\n");
+	printf("Print section header\n");
+	nb = (size_t)file[52];
+	nb2 = (size_t)((file[40]*(file[58] + file[60])));
+	for (size_t i = nb; i < (size_t)(file[52] + file[54]); i++){
+		if (i == nb) printf("4 bytes p_type: ");
+		if (i == nb + 4) printf("4 bytes p_flags: ");
+		if (i == nb + 8) printf("4 bytes p_offset: ");
+		if (i == nb + 16) printf("8 bytes p_vaddr: ");
+		if (i == nb + 24) printf("8 bytes p_paddr: ");
+		if (i == nb + 32) printf("8 bytes p_filesz: ");
+		if (i == nb + 40) printf("8 bytes p_memsz: ");
+		if (i == nb + 48) printf("8 bytes p_align: ");
+		if (lala[i] < 16) printf("0");
+		printf("%x ", file[i]);
+		if (i == nb + 3) printf("\n");
+		if (i == nb + 7) printf("\n");
+		if (i == nb + 15) printf("\n");
+		if (i == nb + 23) printf("\n");
+		if (i == nb + 31) printf("\n");
+		if (i == nb + 39) printf("\n");
+		if (i == nb + 47) printf("\n");
+		if (i == nb + 55) printf("\n");
+	}
+	printf("\n");
+	for (size_t i = nb2 ; i < nb2 + 64; i++){
+		printf("%x ", file[i]);
+	}
+	printf("\n");
+	printf("%ld", nb2);
 }
+	
 
 void printHeader(Elf64_Ehdr  *hdr){
 	printf("HEADER\n");
+	printf("e_ident_EI_MAG 7f 45 4c 46 (valid ELF magic)\n");
 	printf("e_ident: %x\n", hdr->e_ident[0]); // 7f
 	printf("e_ident: %x\n", hdr->e_ident[1]); // 'E'
 	printf("e_ident: %x\n", hdr->e_ident[2]); // 'L'
 	printf("e_ident: %x\n", hdr->e_ident[3]); // 'F'
+	printf("//////////////////////////////\n");
 	printf("e_ident: %d\n", hdr->e_ident[4]); // 2
 	printf("e_ident: %d\n", hdr->e_ident[5]);
 	printf("e_ident: %d\n", hdr->e_ident[6]);
--- main.c
+++ main.c
@@ -1,24 +1,31 @@
 #include "./woodyWoodPacker.h"
 
 void	check_if_file_correct(char *file, void *file_memory){
-	struct stat sb;
-	int					fd;
+	struct stat	sb;
+	int			fd;
 	Elf64_Ehdr	*hdr;
+	char		*hdr2;
 
 	if (!(stat(file, &sb) == 0 && sb.st_mode & S_IXUSR))
 		ft_error_no_executable(file);
 	printStats(&sb);
 	if ((fd = open(file, O_RDONLY)) < 0)
 		ft_error_no_right_to_read(file);
+	if ((hdr2 = malloc(sizeof(char) * sb.st_size + 1)) == NULL)
+		exit(1);
 	file_memory = mmap(0, sb.st_size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+	read(fd, hdr2, sb.st_size);
+	hdr2[sb.st_size] = '\0';
 	close(fd);
 	printFileHexa(file_memory, sb.st_size); //debug
+	printFileHexa(hdr2, sb.st_size); //debug
 	hdr = (Elf64_Ehdr *)file_memory;
 	printHeader(hdr); //debug
 	if (!(strncmp((char *)hdr, ELFMAG, SELFMAG) == 0 && hdr->e_ident[4] == ELFCLASS64))
 		ft_error_wrong_header(file);
 	//e_type : identifie le type de fichier. ex : un exécutable ou un fichier objet partagé
 	// du coup mettre une protection
+	free(hdr2);
 	munmap(file_memory, sb.st_size);
 }
 
